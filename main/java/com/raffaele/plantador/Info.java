38bd1a76e971701e1399265a8b91129b8474f89a
Raffaele Mancino <raffaele.linux@gmail.com>
2016-08-26 18:39:07 +0200

    Info Version Update

--- main/java/com/raffaele/plantador/Info.java
+++ main/java/com/raffaele/plantador/Info.java
@@ -23,7 +23,7 @@ package com.raffaele.plantador;
 public class Info {
     public static final String ID = "plantador";
     public static final String NAME = "Plantador";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.4";
     public static final String MINECRAFT = "1.7.10";
     
 }
