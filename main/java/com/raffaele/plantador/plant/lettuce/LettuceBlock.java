1b430c469a15178ca3a8618dbdfcaa5cd303a2a4
Raffaele Mancino <raffaele.linux@gmail.com>
2017-01-11 13:13:51 +0100

    Lettuce added

--- main/java/com/raffaele/plantador/plant/lettuce/LettuceBlock.java
+++ main/java/com/raffaele/plantador/plant/lettuce/LettuceBlock.java
@@ -19,14 +19,11 @@ package com.raffaele.plantador.plant.lettuce;
 import com.raffaele.plantador.plant.Plant;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import java.util.ArrayList;
-import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
@@ -80,20 +77,6 @@ public class LettuceBlock extends BlockCrops
             return this.level[3];
         }
     }
-    
-    /*@Override
-    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
-    {
-        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
-        if (metadata>=3)
-        {
-            ret.add(new ItemStack(this.func_149865_P(),3));
-        }else{
-            ret.add(new ItemStack(this.func_149866_i()));
-        }
-
-        return ret;
-    }*/
 
     @Override
     public boolean canBlockStay(World world, int x, int y, int z)
